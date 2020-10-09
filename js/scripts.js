2eeba61c0a41ded763fb9d4bdafc4a31ebeba609
Carlos García <67180099+CarlosG4rc@users.noreply.github.com>
2020-10-09 14:54:54 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,97 +1,132 @@
-console.log("1");
-(function(){
+$(function(){
   'use strict';
-  document.addEventListener('DOMContentLoaded', function(){
-    //getElementById
-    // var logo = document.getElementById('logo');
-    // var navegacion = document.getElementById('navegacion');
-    // navegacion.style.display = "none";
+  $('.nuestros-servicios div:first').show();
+  $('.servicios nav a:first').addClass('activo');
 
-    //getElementByClassName
-    // var navegacion = document.getElementsByClassName('navegacion');
-    // console.log(navegacion[1])
-    
-    // var enlaces = document.getElementsByTagName('a');
-    // console.log(enlaces);
-    // for(var i = 0; i< enlaces.length; i++){
-    //   enlaces[i].setAttribute('target', '_blank');
-    // }
-    // var enlacessidebar = document.getElementById('sidebar').getElementsByTagName('a');
-    // for(var i = 0; i< enlacessidebar.length; i++){
-    //   enlacessidebar[i].setAttribute('href', 'http://www.google.com');
-    // }
-    
-    // var logo = document.querySelector('.logo');
-    // console.log(logo);
+  $('.servicios nav a'). on('click', mostrarTabs);
 
-    // var encabezados = document.querySelectorAll('h2, footer p');
-    // console.log(encabezados);
+  function mostrarTabs(){
+    $('.servicios nav a').removeClass('activo');
+    $(this).addClass('activo');
+    var enlace = $(this).attr('href');
+    $('.nuestros-servicios div').hide();
+    $(enlace).show();
 
-    // var enlaces = document.querySelectorAll('a');
-    // for (car i = 0; i < enlace.length; i++){
-    //   console.log(enlaces);
-    // }
+    return false;
+  }
 
-    // var enlaces = document.querySelectorAll('#menu ul li a')[0];
-    // console.log(enlaces.nodeType);
-    // console.log(enlaces.nodeName);
-    // console.log(enlaces.attributes);
-    // console.log(enlaces.firstChild);
-    // console.log(enlaces.firstChild.nodeValue);
-    // enlaces.firstChild.nodeValue = "Home";
-    // 
+/* var proximosViajes = ['Londres', 'Valencia', 'Madrid', 'Paris', 'Milan'];
+  $.each(proximosViajes, function(i, v){ //i= indice, v=valor
+    if(i == 0){
+      $('aside').append('<h2>Próximos viajes</h2>')
+    }
+    $('aside').append('<li>' + v + '</li>');
+  })
+  var viajesPorFecha = {
+    primero:'Londres',
+    segundo:'Valencia',
+    tercero:'Madrid',
+    cuarto:'Paris',
+    quinto:'Milan'
 
-    // var sidebar = document.querySelector('#sidebar');
-    // var nuevoElemento = document.createElement("H1");
-    // var nuevoTexto = document.createTextNode("Hola Mundo");
-    // nuevoElemento.appendChild(nuevoTexto);
-    // sidebar.appendChild(nuevoElemento);
-
-    // var enlacesSidebar = document.querySelectorAll('#sidebar ul');
-    // var nuevoEnlace = document.createElement('A');
-    // nuevoEnlace.setAttribute('href', 'http://www.google.com');
-    // var textoEnlace = document.createTextNode('Entrada 6');
-    // nuevoEnlace.appendChild(textoEnlace);
-    // var nuevaLista = document.createElement('LI');
-    // nuevaLista.appendChild(nuevoEnlace);
-    // enlacesSidebar[0].appendChild(nuevaLista);
-
-    // var contenido = document.querySelectorAll('main');
-    // var nuevoContenido = contenido[0].cloneNode(true);
-    // var sidebar = document.querySelector('aside');
-    // sidebar.insertBefore(nuevoContenido, sidebar.childNodes[5]);
-
-    // var sidebar = document.querySelector('aside');
-    // var masVisitados = document.createElement('H2');
-    // var textoVisitados = document.createTextNode('Post más visitados');
-    // masVisitados.appendChild(textoVisitados);
-    // sidebar.insertBefore(masVisitados, sidebar.childNodes[0]);
-
-    // var contenido = document.querySelectorAll('main h2');
-    // for (var i = 0; i < contenido.length; i++){
-    //   var nuevoElemento = document.createElement('LI');
-    //   var nuevoTexto = document.createTextNode(contenido[i].firstChild.nodeValue);
-    //   nuevoElemento.appendChild(nuevoTexto);
-    //   sidebar.insertBefore(nuevoElemento, sidebar.childNodes[1]);
-    // }
-
-    // var primerPost = document.querySelector('main article');
-    // primerPost.parentNode.removeChild(primerPost);
-
-    // var enlaces =document.querySelector('#navegacion nav ul li a');
-    // console.log(enlaces);
-    // enlaces.parentNode.removeChild(enlaces);
-
-    // var viejoNodo = document.querySelector('main h2');
-    // var nuevoNodo = document.querySelector('footer h2');
-    // viejoNodo.parentNode.replaceChild(nuevoNodo,viejoNodo);
-
-    var nuevoTitulo = document.createElement('H2');
-    var nuevoTexto = document.createTextNode('Hola mundo');
-    nuevoTitulo.appendChild(nuevoTexto);
-
-    var viajeNodo = document.querySelector('main h2');
-    viajeNodo.parentNode.replaceChild(nuevoTitulo,viajeNodo);
-
+  };
+  $.each(viajesPorFecha, function(i,v){
+    $('aside').append('<li>' + i + '-' + v + '</li>')
   });
-})();
+  
+  $('div.logo img').on('click', cargarAjax);
+
+function cargarAjax(){
+  $.ajax('promociones.txt', {
+    success: agregarContenido,
+    type:'GET',
+    datatype:'text'
+  });
+}
+function agregarContenido(data, status, jqxhr){
+  $('aside').text(data);
+  console.log(status)
+}
+
+  $('.logo img').click(function(){
+    $('main article:first').slideUp(1000); //1000=1seg
+  });
+  $('aside').click(function(){
+    $('main article:first').slideDown(1000); //1000=1seg
+  });
+  $('.logo img').on('click', function(){
+    $(this).animate({'width':'200px'}, 1000);
+  });
+
+  $('main article img').on('mouseenter', aumentarimagen);
+  $('main article img').on('mouseleave',disminuirimagen);
+
+  function aumentarimagen(){
+    $(this).animate({'width':'100%'});
+  }
+  function disminuirimagen(){
+    $(this).animate({'width':'350px'});
+  }
+
+  $('.logo img').css({'width':'400px'});
+
+  $('main article h2').css({'color':'#db008d'})
+
+  $('aside').css({'background-color':'#e1e1e1',
+                  'text-transform': 'uppercase',
+                'padding': '2px'})
+
+  $('.navegacion ul li a').on('mouseenter', cambiarColor);
+  function cambiarColor(){
+    $('.navegacion').css({'background-color': 'red'});
+  }
+
+  $('main article:first img').on('click',function(){
+    $(this).attr('src','img/imagen_2.jpg');
+  });
+
+  $('main').on({
+    click: function(){
+      $(this).addClass('fondorojo activo');
+    }, mouseenter: function(){
+      $(this).addClass('fondorojo');
+    }, mouseleave: function(){
+      $(this).addClass('activo');
+    }
+  })
+
+  $('div.logo img').addClass('activo');
+  $('.navegacion').show();
+  // $('.navegacion nav ul li:first').addClass('activo');
+
+  $('.navegacion ul li a').on('click',function(e){
+    $('.navegacion ul li a').removeClass('activo');
+    e.preventDefault();
+    $(this).addClass('activo');
+  })
+
+  $('#menu').on('click',function(){
+    $('#navegacion').show();
+  });
+  
+  $('div.logo img').on('mouseenter', function(){
+    console.log("Sobre el logo")
+  });
+  $('div.logo img').on('mouseleave', function(){
+    console.log("Fuera del logo")
+  });
+
+  $('div.logo img').on('click', function(){
+    $(this).remove();
+  })
+
+  $ ('main article:first').remove();
+
+  var copia =  $('main article:last').clone();
+  $('main').prepend(copia);
+  var copia =  $('main article:last').clone();
+  $(copia).prependTo('main');
+  $('div.logo img').on('click', function(){
+    console.log("Has hecho click en el logo")
+  })*/
+});
